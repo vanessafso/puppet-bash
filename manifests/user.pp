c98c20f3616e10678d440396c94d025eb127e19a
Danny Roberts <danny.roberts@reconnix.com>
2015-03-04 11:37:16 +0000

    resource name will have to be the username, no way around it that I can see

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,16 +1,15 @@
 define bashprofile::user (
-  $user                  = $name,
   $homedir               = "/home/$name",
   $environment_variables = {},
 ) {
 
-  validate_string($user)
+  validate_string($name)
   validate_absolute_path($homedir)
   validate_hash($environment_variables)
 
   file { "${homedir}/.bash_profile":
     ensure  => 'file',
-    owner   => $user,
+    owner   => $name,
     mode    => '0644',
     content => template('bashprofile/user-profile.erb'),
   }
