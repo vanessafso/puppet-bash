3a87eacaa0766a9a7aea80a122207998a3841d43
Danny Roberts <danny.roberts@reconnix.com>
2015-03-04 10:52:25 +0000

    Added preliminary user profile support

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,7 +1,18 @@
 define bashprofile::user (
-
+  $user                  = $name,
+  $homedir               = "/home/${user}"
+  $environment_variables = {},
 ) {
 
+  validate_string($user)
+  validate_absolute_path($homedir)
+  validate_hash($environment_variables)
 
+  file { "${homedir}/.bash_profile":
+    ensure  => 'file',
+    owner   => $user,
+    mode    => '0644',
+    content => template('bashprofile/user-profile.erb'),
+  }
 
 }
