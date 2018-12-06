3dac9b2bf3f6229d9732681061949c8c8f249d5b
vanessafso <vanessasouza@algartelecom.com.br>
2018-12-06 16:25:08 -0200

    Adicionado a variavel user_group para evitar que arquivo .bash_profile seja criado com grupo diferente do usuario

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,17 +1,30 @@
 define bash::user (
-  $homedir               = "/home/${name}",
+  $homedir       = "/home/${name}",
   $env_variables = {},
+  $user_group    = undef,
 ) {
 
   validate_string($name)
   validate_absolute_path($homedir)
   validate_hash($env_variables)
 
-  file { "${homedir}/.bash_profile":
-    ensure  => 'file',
-    owner   => $name,
-    mode    => '0644',
-    content => template('bash/user-profile.erb'),
+  if $user_group == undef {
+    file { "${homedir}/.bash_profile":
+      ensure  => 'file',
+      owner   => $name,
+      group   => $name,
+      mode    => '0644',
+      content => template('bash/user-profile.erb'),
+    }
+
+  } else {
+    file { "${homedir}/.bash_profile":
+      ensure  => 'file',
+      owner   => $name,
+      group   => $user_group,
+      mode    => '0644',
+      content => template('bash/user-profile.erb'),
+    }
   }
 
 }
